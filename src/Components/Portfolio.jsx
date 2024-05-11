41e9c59e27840eca274dca1561519b81d45fe1e7
Fathma Mehnoor <fathmamehnoor@gmail.com>
2024-05-11 14:15:58 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/adelin-preda--tOr_T4qTpQ-unsplash.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Night sky pattern";
 
 /**
  * Project list
@@ -31,28 +31,22 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Use of Libcurl to contact remote servers",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Integrated 'libcurl' library to Gnuastro to enhance remote server access.",
+    url: "https://github.com/fathmamehnoor/gnuastro",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Writing of TIFF files",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Enhanced the functionality of Gnuastro by implementing the capability to write TIFF files..",
+    url: "https://github.com/fathmamehnoor/gnuastro",
   },
   {
     title: "My Resume Site",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    url: "https://github.com/fathmamehnoor/pro9",
   },
 ];
 
